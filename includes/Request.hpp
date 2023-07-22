768d4125f28c4d1e952b7e57a402f94121207d86
omanar <omanar@student.1337.ma>
2023-07-22 13:39:52 +0100

    parsing request done

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: omanar <omanar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 11:17:52 by omanar            #+#    #+#             */
-/*   Updated: 2023/07/20 21:40:08 by omanar           ###   ########.fr       */
+/*   Updated: 2023/07/22 13:34:30 by omanar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ class Request {
 		std::string _method;
 		std::string _url;
 		std::string _version;
-		std::string _host;
 		std::string _userAgent;
+		std::string _host;
+		std::string _contentLength;
+		std::string _contentType;
 		std::string _body;
 	public:
 		Request(std::string const &request);
@@ -29,9 +31,13 @@ class Request {
 		std::string const &getMethod() const;
 		std::string const &getUrl() const;
 		std::string const &getVersion() const;
-		std::string const &getHost() const;
 		std::string const &getUserAgent() const;
+		std::string const &getHost() const;
+		std::string const &getContentLength() const;
+		std::string const &getContentType() const;
 		std::string const &getBody() const;
+		std::string requestParser(std::string const &request, std::string const &directive);
+		std::string bodyParser(std::string const &request);
 		
 		void display() const;
 };
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -6,54 +6,55 @@
 /*   By: omanar <omanar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 11:43:05 by omanar            #+#    #+#             */
-/*   Updated: 2023/07/21 19:02:02 by omanar           ###   ########.fr       */
+/*   Updated: 2023/07/22 13:35:29 by omanar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
 
-Request::Request(std::string const &request) {
+std::string Request::requestParser(std::string const &request, std::string const &directive) {
+	size_t pos = request.find(directive);
+	if (pos == std::string::npos)
+		throw std::runtime_error("bad request");
+	size_t start = pos + directive.size();
+	size_t end = request.find("\r\n", start);
+	if (end == std::string::npos)
+		throw std::runtime_error("bad request");
+	return (request.substr(start, end - start));
+}
 
-	std::cout << " ---------- Request ---------------\n[" << request << "]" << std::endl;
-	std::cout << " ----------------------------------\n" << std::endl;
+std::string Request::bodyParser(std::string const &request) {
+	size_t pos = request.find("\r\n\r\n");
+	if (pos == std::string::npos)
+		throw std::runtime_error("bad request");
+	size_t start = pos + 4;
+	size_t end = request.size();
+	if (end == std::string::npos)
+		throw std::runtime_error("bad request");
+	return (request.substr(start, end - start));
+}
+
+Request::Request(std::string const &request) {
 
 	size_t pos = request.find("\r\n");
 	if (pos == std::string::npos)
-		throw std::runtime_error("bad requ1est");
+		throw std::runtime_error("bad request");
 	std::string line = request.substr(0, pos);
 	std::istringstream iss(line);
 	if ((iss >> _method >> _url >> _version).fail())
-		throw std::runtime_error("bad requ2est");
+		throw std::runtime_error("bad request");
 	if (_method != "GET" && _method != "POST" && _method != "DELETE")
 		throw std::runtime_error("Method not supported");
 	if (_version != "HTTP/1.1")
 		throw std::runtime_error("Version not supported");
-	size_t end = request.find("\r\n", pos + 2);
-	if (end == std::string::npos)
-		throw std::runtime_error("bad requ3est");
-	pos = request.find("Host: ");
-	if (pos == std::string::npos)
-		throw std::runtime_error("bad re4quest");
-	size_t start = pos + 6;
-	end = request.find("\r\n", start);
-	if (end == std::string::npos)
-		throw std::runtime_error("bad reque5st");
-	_host = request.substr(start, end - start);
-	pos = request.find("User-Agent: ");
-	if (pos == std::string::npos)
-		throw std::runtime_error("bad reque6st");
-	start = pos + 12;
-	end = request.find("\r\n", start);
-	if (end == std::string::npos)
-		throw std::runtime_error("bad req7uest");
-	_userAgent = request.substr(start, end - start);
-	pos = request.find("\r\n\r\n");
-	if (pos == std::string::npos)
-		throw std::runtime_error("bad re8quest");
-	start = pos + 4;
-	end = request.find("\r\n", start);
-	if (end == std::string::npos)
-		_body = request.substr(start, request.length() - start);
+
+	_host = requestParser(request, "Host: ");
+	_userAgent = requestParser(request, "User-Agent: ");
+	if (_method == "POST") {
+		_contentLength = requestParser(request, "Content-Length: ");
+		_contentType = requestParser(request, "Content-Type: ");
+		_body = bodyParser(request);
+	}
 }
 
 Request::~Request() {}
@@ -64,9 +65,13 @@ std::string const &Request::getUrl() const { return _url; }
 
 std::string const &Request::getVersion() const { return _version; }
 
+std::string const &Request::getUserAgent() const { return _userAgent; }
+
 std::string const &Request::getHost() const { return _host; }
 
-std::string const &Request::getUserAgent() const { return _userAgent; }
+std::string const &Request::getContentLength() const { return _contentLength; }
+
+std::string const &Request::getContentType() const { return _contentType; }
 
 std::string const &Request::getBody() const { return _body; }
 
@@ -74,7 +79,9 @@ void Request::display() const {
 	std::cout << "Method: " << _method << std::endl;
 	std::cout << "Url: " << _url << std::endl;
 	std::cout << "Version: " << _version << std::endl;
-	std::cout << "Host: " << _host << std::endl;
 	std::cout << "User-Agent: " << _userAgent << std::endl;
+	std::cout << "Host: " << _host << std::endl;
+	std::cout << "Content-Length: " << _contentLength << std::endl;
+	std::cout << "Content-Type: " << _contentType << std::endl;
 	std::cout << "Body: " << _body << std::endl;
 }
